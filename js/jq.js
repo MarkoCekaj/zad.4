0a1881f7ebfa572b36782922ed2d385257cad86f
Marko Cekaj <marko.cekaj@amplitudo.me>
2021-03-23 15:49:10 +0100

    minor changes

--- js/jq.js
+++ js/jq.js
@@ -1,13 +1,14 @@
 function bookSearch() {
   var search = document.getElementById("search").value;
   document.getElementById("results").innerHTML = "";
+  table = document.getElementById("results-table");
   console.log(search);
   $.ajax({
     url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
     dataType: "json",
     success: function (data) {
       for (i = 0; i < data.items.length; i++) {
-        results.innerHTML += "<h2>" + data.items[i].volumeInfo.title + "</h2>";
+        results.innerHTML += "<td>" + data.items[i].volumeInfo.title + "</td>";
       }
     },
     type: "GET",
