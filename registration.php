24e94185ddfc909b060f1de810d6f8363fa4e88f
Marko Cekaj <marko.cekaj@amplitudo.me>
2021-03-23 15:47:23 +0100

    sign up php registration

---
+++ registration.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+$con = mysqli_connect('localhost', 'root', '');
+mysqli_select_db($con, 'userregistration');
+$email = $_POST['email'];
+$pass = $_POST['password'];
+$s = "select * from usertable where email = '$email'";
+$result = mysqli_query($con, $s);
+$num = mysqli_num_rows($result);
+if ($num == 1) {
+    echo "username taken";
+} else {
+
+    $reg = "insert into usertable(email , password) values ('$email', '$pass')";
+    mysqli_query($con, $reg);
+    echo "reggg succc";
+    header("Location: login.php");
+}
