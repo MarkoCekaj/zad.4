741d130beb2061f42a157132672de53b999ab0de
Marko Cekaj <marko.cekaj@amplitudo.me>
2021-03-25 13:56:37 +0100

    contact validation php

---
+++ contact.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$con = mysqli_connect('localhost', 'root', '', 'zad4_baza');
+
+// get the post records
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$message = $_POST['subject'];
+
+// database insert SQL code
+$sql = "INSERT INTO `kontakt_poruke` (`id`, `fldIme`, `fldPrezime`, `fldPoruka`) VALUES ('0', '$firstname', '$lastname', '$message')";
+
+// insert in database 
+$rs = mysqli_query($con, $sql);
+
+if ($rs) {
+    echo "Contact Records Inserted";
+    header('Location: ./index.html');
+}
